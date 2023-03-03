26a6466dac514f8d7fba8ce91dd563e3f5f88561
Andrei <andrei.hudalla@outlook.com>
2023-03-03 19:47:18 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+  <!---- Header ---->
+  <header>
+
+  </header>
+  <!---- Main ---->
+  <main>
+
+  </main>
+  <!---- Footer ---->
+  <footer>
+
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
